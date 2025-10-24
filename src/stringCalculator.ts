8905535e3651e74f50b85fee586d418bedcf4b75
Akhil <akhilxavier95@gmail.com>
2025-10-24 20:15:09 +0530

    feat: handle single number string input

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,7 +1,9 @@
 export const calculateString = (input: string): number => {
-  if (input === "") {
-    return 0;
-  }
+  if (input === "") return 0;
+
+  const n = Number(input);
+  if (!isNaN(n)) return n;
+
   let sum = 0;
   return sum;
 };
